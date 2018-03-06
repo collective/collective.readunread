0bd89db881c27c7c19211cffca1d245837361493
Stephan Geulette <s.geulette@imio.be>
2018-03-06 12:21:27 +0100

    Translated buttons. Added colors.

--- src/collective/readunread/locales/update.sh
+++ src/collective/readunread/locales/update.sh
@@ -9,4 +9,5 @@
 domain=collective.readunread
 
 i18ndude rebuild-pot --pot $domain.pot --create $domain ../
+i18ndude merge --pot $domain.pot --merge $domain-manual.pot 2>/dev/null
 i18ndude sync --pot $domain.pot */LC_MESSAGES/$domain.po
